953928151ca17465f1b1e23572c8b406a375f1a0
Yechiam Weiss <yyyopla3211@gmail.com>
2021-10-25 17:53:22 +0300

    Summary Route initialized. Need to implement rating, like buttons, as well as discussion, article linking, and more.

--- app-dir/src/MainScreen/SummaryView/SummaryRoute.tsx
+++ app-dir/src/MainScreen/SummaryView/SummaryRoute.tsx
@@ -1,12 +1,35 @@
 import React from "react";
-import { Label } from "semantic-ui-react";
+import { Card, Grid, Rating } from "semantic-ui-react";
+import { usePostsContext } from "../../Context/Store";
 
 
 export const SummaryRoute = ():JSX.Element => {
 
+    const {state: {selectedPost}} = usePostsContext();
+
     return(
-        <>
-            <Label>HI</Label>
-        </>
+        <Grid columns={4} verticalAlign="middle" textAlign="center">    {/* idk why verticalAlign doesn't work */}
+            <Grid.Row>
+                <Grid.Column>
+                    <Card raised centered>
+                        <Card.Content header={(typeof selectedPost.article === 'object') ? 
+                                        selectedPost.article.title /* I NEED TO PUT TITLE FOR SUMMARY!!! */ : '' } />
+                        <Card.Content description={selectedPost.summary} />
+                        <Card.Content meta={selectedPost.publishDate} />
+                        <Card.Content extra textAlign="left">
+                            <Rating icon='star' defaultRating={selectedPost.rating} maxRating={5} onRate={()=>{console.log('rated')}} />
+                            <span style={{float: "right"}}>{"Likes: "  + selectedPost.likes}</span>
+                        </Card.Content>
+                        {selectedPost.tags && selectedPost.tags.length > 0 ? 
+                            <Card.Content extra>
+                                {selectedPost.tags}
+                            </Card.Content>
+                            :
+                            <></>
+                        }
+                    </Card>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
     );
 }
