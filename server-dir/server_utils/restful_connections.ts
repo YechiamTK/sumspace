9814e9f753468fec51187edca18af10cb6c258fd
Yechiam Weiss <yyyopla3211@gmail.com>
2021-11-06 23:33:37 +0200

    Made big progress with comments, but I might need to rewrite. Currently addressing comments as nested doc but it isn't actually a doc? Need to further research.

--- server-dir/server_utils/restful_connections.ts
+++ server-dir/server_utils/restful_connections.ts
@@ -7,7 +7,7 @@
 import { Router, Request, Response } from 'express';
 import { LeanDocument, Mongoose, ObjectId } from 'mongoose';
 import { UserBE, ArticleBE, AuthorBE, SummaryBE, TagBE,
-  retrieveAuthorSchema, retrieveUserSchema, retrieveSummarySchema, retrieveArticleSchema, retrieveTagSchema } from './schemas';
+  retrieveAuthorSchema, retrieveUserSchema, retrieveSummarySchema, retrieveArticleSchema, retrieveTagSchema, retrieveCommentSchema, CommentBE } from './schemas';
 
 
 /**
@@ -417,6 +417,38 @@ export async function registerUser(router: Router, mongoose: Mongoose){
 }
 
 /**
+ * util function fullSummary
+ * 
+ * returns fully populated summary
+ * 
+ * expects summary id
+ * 
+ * @param router express router to be used
+ * @param mongoose mongoose object
+ */
+export async function fullSummary(router: Router, mongoose: Mongoose){
+  router.post('/full-summary', (req: Request, res: Response)=>{
+    const { summaryId } = req.body.params;
+
+    const summaryModel = mongoose.models.Summary || mongoose.model('Summary', retrieveSummarySchema(mongoose));
+    const userModel = mongoose.models.User || mongoose.model('User', retrieveUserSchema(mongoose));
+    const CommentModel = mongoose.models.Comment || mongoose.model('Comment', retrieveCommentSchema(mongoose));
+    const ArticleModel = mongoose.models.Article || mongoose.model('Article', retrieveArticleSchema(mongoose));
+    const TagModel = mongoose.models.Tag || mongoose.model('Tag', retrieveTagSchema(mongoose));
+
+    summaryModel.findOne({'_id': summaryId})
+                .populate('user').populate('comments.comment').populate('article').populate('tags')
+                .lean().exec().then((result: LeanDocument<SummaryBE>)=>{
+                  console.log(result);
+                  res.send(result);
+        }).catch((err)=>{
+          console.log(err);
+      });
+  })
+}
+
+
+/**
  * util function findTagsOid:
  * 
  * searches for the requested tags and returns their oids.
@@ -506,6 +538,59 @@ export async function registerUser(router: Router, mongoose: Mongoose){
       console.log("new-tags: error occured: " + err);
     });
   });
+}
+
+/**
+ * util function newComment
+ * 
+ * save new comment in the summary.
+ * 
+ * @param router 
+ * @param mongoose
+ */
+export async function newComment(router: Router, mongoose: Mongoose){
+  router.post('/new-comment', async (req: Request, response: Response)=>{
+    console.log("entered new-comment");
+    const { userId, commentText, summaryId } = req.body.params;
+
+    const commentModel = mongoose.models.Comment || mongoose.model('Comment', retrieveCommentSchema(mongoose));
+    console.log("new-comment: connected to comment model");
+    const summaryModel = mongoose.models.Summary || mongoose.model('Summary', retrieveSummarySchema(mongoose));
+    console.log("new-comment: connected to summary model");
+
+    //create the comment
+    const comment = new commentModel({
+      user: userId,
+      text: commentText,
+      date: new Date().toLocaleDateString(),
+      likes: 0
+    });
+    
+    //connect to the summary
+    await summaryModel.findOne({'_id': summaryId}).exec().then(async (result: any)=>{ //any because the document type isn't good enough
+      (result.comments ?
+        //if the comments exists, simply push the comment
+        (result.comments.push(comment))
+        :
+        //otherwise create the array first, then push
+        (result.comments = new Array<CommentBE>(),
+        result.comments.push(comment))
+      )
+      
+      //save as subdoc to summary
+      await result.save((err:any,doc: SummaryBE)=>{
+        if (err){
+          console.log("new-comment: An error has accured: " + err);
+          response.send("error occured");
+        }
+        else{
+          console.log("new-comment: Comment saved successfuly!");
+          response.send("success");
+        }
+      });
+    });
+  });
+
 }
 
 
